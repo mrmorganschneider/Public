b2a9553d533e5ac0e51be459c84809c1fd7b5c04
Morgan Schneider <mr.cardholder.osi@gmail.com>
2020-12-23 12:33:48 -0600

    Exercise loops working example

--- GO/exercise_1.go
+++ GO/exercise_1.go
@@ -10,9 +10,24 @@ import (
 func Sqrt(x float64) float64 {
 
 	z := float64(1)
+
+	//method for 10 iterations
 	//for i := 1; i <= 10; i++ {
 	//	z = z - (z*z-x)/(2*z)
 	//}
+
+	//method for programable iterations
+	//change diff for smaller differences
+
+	diff := 0.0000001
+
+	for {
+		z = z - (z*z-x)/(2*z)
+		fmt.Println(z)
+		if (z*z-x)/(2*z) < diff {
+			break
+		}
+	}
 
 	return z
 }
