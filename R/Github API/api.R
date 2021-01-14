477830e1ba0116dfe6e370e9468fe6de24b963aa
Morgan Schneider <mr.cardholder.osi@gmail.com>
2021-01-14 17:16:11 -0600

    updated with sql answers

--- R/Github API/api.R
+++ R/Github API/api.R
@@ -1,10 +1,12 @@
 library(httr)
+library(jsonlite)
+library(httpuv)
 
 oauth_endpoints("github")
 
-myapp <- oauth_app("github",
+myapp <- oauth_app("Mschneider week 2 test api",
   key = "1df5f08d3846099db9c4",
-  secret = "32f5941ee3e3bf320955bcd27eadc7916f067207"
+  secret = "3e9d4a7885b73cd5e7ef612f1058bf0b7d08021d"
 )
 
 # 3. Get OAuth credentials
@@ -13,5 +15,8 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 # 4. Use API
 gtoken <- config(token = github_token)
 req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
 stop_for_status(req)
-content(req)
+json_out = content(req)
+
+DF = jsonlite::fromJSON(jsonlite::toJSON(json_out))
